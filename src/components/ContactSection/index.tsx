1a9cdb22eacd70c6df2ca8ff2ed768f5361fee89
Lennon <lennon_ximenes@hotmail.com>
2023-12-07 11:55:46 -0300

    feat - The contact section has been created and stylized

--- src/components/ContactSection/index.tsx
+++ src/components/ContactSection/index.tsx
@@ -1,5 +1,8 @@
 import { FontParagraph, FontTitle } from "../../styles/typograph"
 import { StyledContactSection } from "./styled"
+import { FaWhatsapp } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa6";
 
 export const ContactSection = () => {
     return (
@@ -8,6 +11,32 @@ export const ContactSection = () => {
                 <FontTitle>Let's set up a conversation and <span>develop our creativity</span> together?</FontTitle>
                 <FontParagraph>Advertise your brand organically within Dribbble’s design inspiration feed.</FontParagraph>
             </div>
+            <ul className="myContacts">
+                <li>
+                    <FaWhatsapp size={35} color="#623CEA" />
+                    <div>
+                        <FontTitle>My phone</FontTitle>
+                        <FontParagraph>+5524992470744</FontParagraph>
+                    </div>
+                </li>
+
+                <li>
+                    <MdOutlineEmail size={35} color="#623CEA" />
+                    <div>
+                        <FontTitle>My email</FontTitle>
+                        <FontParagraph>lennon_ximenes@hotmail.com</FontParagraph>
+                    </div>
+                </li>
+
+                <li>
+                    <FaLinkedinIn size={35} color="#623CEA" />
+                    <div>
+                        <FontTitle>My linkedin</FontTitle>
+                        <FontParagraph><a href="https://www.linkedin.com/in/lennon-ximenes/" target="_blank">Lennon Ximenes</a> </FontParagraph>
+                    </div>
+                </li>
+
+            </ul>
 
         </StyledContactSection>
     )
--- src/components/ContactSection/styled.ts
+++ src/components/ContactSection/styled.ts
@@ -4,6 +4,68 @@ export const StyledContactSection = styled.section`
     height: auto;
     width: 100%;
 
-    .titleParagraphContact {}
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content:space-around;
+    align-items: center;
+
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+
+    .titleParagraphContact {
+        height: auto;
+        width: fit-content;
+
+        display: flex;
+        flex-direction: column;
+
+        h1 {
+            width: 100%;
+            text-align: center;
+
+            span {
+                color: var(--color-primary);
+            }
+        }
+
+        p {
+            width: 100%;
+            text-align: center;
+
+            padding-top: 2rem;
+            padding-bottom: 2rem;
+        }
+    }
+
+    .myContacts {
+
+        li {
+            display: flex;
+            flex-direction: row;
+
+            padding-bottom: 2rem;
+
+            div {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-around;
+
+                padding-left: 1rem;
+
+                h1 {
+                    text-align: left;
+                }
+
+                p {
+
+                    a {
+                        text-decoration: none;
+                        color: var(--color--grey-1);
+                    }
+                }
+            }
+        }
+    }
 
 `
