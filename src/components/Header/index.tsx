f78526f6a04538e79f102cc22800ffc5339c3e8c
Lennon <lennon_ximenes@hotmail.com>
2023-12-14 12:33:47 -0300

    style - Styling for responsive menu and addition of dynamic button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,70 @@
+import { useState, useEffect, useRef } from "react";
 import { FontA, FontTitle } from "../../styles/typograph";
 import { StyledHeader, StyledMenu } from "./styled";
-// import { RiMenuAddFill } from "react-icons/ri";
+import { CiMenuBurger } from "react-icons/ci";
 
 export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [showButton, setShowButton] = useState(true);
+    const menuRef = useRef(null);
+
+    const handleResize = () => {
+        if (window.innerWidth <= 767) {
+            setIsOpen(false);
+            setShowButton(true);
+        } else {
+            setIsOpen(true);
+            setShowButton(false);
+        }
+    };
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        
+        document.addEventListener("click", handleOutsideClick);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleOutsideClick = (e) => {
+        if (menuRef.current && !menuRef.current.contains(e.target)) {
+            setIsOpen(false);
+        }
+    };
+
     return (
         <StyledHeader>
             <div>
                 <FontTitle>Lennon Ximenes</FontTitle>
             </div>
 
-            <StyledMenu>
-                {/* <button>
-                    <RiMenuAddFill size={30} color="white" />
-                </button> */}
+            <StyledMenu ref={menuRef} isOpen={isOpen}>
+                {showButton && (
+                    <button onClick={toggleMenu}>
+                        <CiMenuBurger size={30} color="white" />
+                    </button>
+                )}
+
                 <ul>
-                    <li><FontA href="#home">Home</FontA></li>
-                    <li><FontA href="#projects">Projects</FontA></li>
-                    <li><FontA href="#contacts">Contacts</FontA></li>
-                    <li><FontA href="#dm">Digital Media</FontA></li>
+                    <li>
+                        <FontA href="#home">Home</FontA>
+                    </li>
+                    <li>
+                        <FontA href="#projects">Projects</FontA>
+                    </li>
+                    <li>
+                        <FontA href="#contacts">Contacts</FontA>
+                    </li>
+                    <li>
+                        <FontA href="#dm">Digital Media</FontA>
+                    </li>
                 </ul>
             </StyledMenu>
         </StyledHeader>
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -2,17 +2,13 @@ import styled from "styled-components";
 
 export const StyledHeader = styled.header`
     position: fixed;
-
     height: 4.375rem;
     width: 100%;
-
     display: flex;
     flex-direction: row;
-    justify-content:space-around;
+    justify-content: space-around;
     align-items: center;
-
     background-color: var(--color-black);
-
     border-bottom: .1575rem solid var(--color-white);
 
     div {
@@ -20,15 +16,20 @@ export const StyledHeader = styled.header`
             color: var(--color-white);
         }
     }
-
 `;
 
 export const StyledMenu = styled.nav`
     display: flex;
     flex-direction: row;
     justify-content: center;
+    align-items: center;
+
+    button {
+        display: none;
+    }
 
     ul {
+        height: auto;
         display: flex;
         flex-direction: row;
         justify-content: center;
@@ -39,8 +40,7 @@ export const StyledMenu = styled.nav`
             flex-direction: row;
             justify-content: space-around;
             align-items: center;
-
-            padding-left: 1rem;
+            padding: 0.5rem;
 
             a {
                 color: var(--color--grey-1);
@@ -53,4 +53,27 @@ export const StyledMenu = styled.nav`
         }
     }
 
+    @media (max-width: 767px) {
+        button {
+            display: block;
+            position: fixed;
+        }
+
+        ul {
+            position: absolute;
+            top: 4.375rem;
+            width: 10rem;
+
+            display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+            flex-direction: column;
+            
+            margin-right: 4.5rem;
+            padding-top: 1rem;
+            padding-bottom: 1rem;
+            padding-left: 0.5rem;
+            
+            border: .0625rem solid var(--color--grey-2);
+            background-color: var(--color-black);
+        }
+    }
 `
